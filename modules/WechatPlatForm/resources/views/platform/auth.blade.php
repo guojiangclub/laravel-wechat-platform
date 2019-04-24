5beb5b53e245bd80fea5a736635d1f67eb67d719
iyoyo dev server <peter@iyoyo.cc>
2019-04-24 16:05:11 +0800

    update

--- modules/WechatPlatForm/resources/views/platform/auth.blade.php
+++
@@ -1,4 +0,0 @@
-<input type="hidden" id="redirect" value="{{$redirect_url}}"></input>
-<script language="javascript" type="text/javascript">
-    window.location.href = document.getElementById('redirect').value;
-</script>
